b939b16509870c796db55079082ccb9fc04b3679
Alexey Rozhin <alexey.rozhin90@gmail.com>
2015-02-01 01:06:18 +0300

    ~ player can jump on platforms.

---
+++ Assets/scripts/PlatformContactController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformContactController : MonoBehaviour
+{
+	private BoxCollider2D platformCollider;
+	private CircleCollider2D playerCollider;
+
+	// Use this for initialization
+	void Start () 
+	{
+		platformCollider = GetComponent <BoxCollider2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		HandlePlatformCollider (other);
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		HandlePlatformCollider (other);
+	}
+
+	void HandlePlatformCollider(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			playerCollider = other.gameObject.GetComponent <CircleCollider2D> ();
+			
+			if (playerCollider.transform.position.y < platformCollider.transform.position.y)
+			{
+				if (!platformCollider.isTrigger)
+				{
+					platformCollider.isTrigger = true;
+				}
+				
+			}else
+			{
+				if (platformCollider.isTrigger)
+				{
+					platformCollider.isTrigger = false;
+				}
+			}
+		}
+
+	}
+}
