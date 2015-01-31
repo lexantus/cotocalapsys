47d4aef40611d8920bddf248287ae98d53171747
Alexey Rozhin <alexey.rozhin90@gmail.com>
2015-01-31 16:24:31 +0300

    ~ second commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	Animator anim;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	void FixedUpdate () 
+	{
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+		{
+			Flip ();
+		}
+		else if (move < 0 && facingRight)
+		{
+			Flip ();
+		}
+	}
+
+	void Flip()
+	{
+		Debug.Log ("Flip");
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
