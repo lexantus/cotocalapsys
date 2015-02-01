86ded7cee80eb8e2672f30c3e6d74dfb990709bb
Alexey Rozhin <alexey.rozhin90@gmail.com>
2015-02-01 15:17:17 +0300

    + shit generator (not finished - in process)

---
+++ Assets/scripts/ShitGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShitGenerator : MonoBehaviour {
+
+	public Player player;
+	public GameObject shit;
+
+	public float startWait;
+	public float makeShitWait;
+
+	public float distanceXFromCatAss;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//StartCoroutine (
+	}
+	
+	IEnumerator MakeShit()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		while (true)
+		{
+			Vector2 shitPosition = new Vector2 (gameObject.transform.x - distanceXFromCatAss, gameObject.transform.y);
+			Instantiate(shit, shitPosition);
+			yield return new WaitForSeconds (makeShitWait);
+		}
+	}
+}
