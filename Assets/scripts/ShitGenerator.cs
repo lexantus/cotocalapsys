22486a04647661075b65ecd10e10fdf6bf2fc0a2
Alexey Rozhin <alexey.rozhin90@gmail.com>
2015-02-01 17:44:04 +0300

    player can make shit of different sizes

--- Assets/scripts/ShitGenerator.cs
+++ Assets/scripts/ShitGenerator.cs
@@ -1,30 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShitGenerator : MonoBehaviour {
-
-	public Player player;
+public class ShitGenerator : MonoBehaviour
+{
 	public GameObject shit;
+//	public Camera camera;
 
 	public float startWait;
 	public float makeShitWait;
 
-	public float distanceXFromCatAss;
+	public float distanceXFromProducerAss;
+	public float sizeShitFactor;
 
-	// Use this for initialization
+	private float shitScale;
+
 	void Start ()
 	{
-		//StartCoroutine (
+		StartCoroutine (MakeShit ());
 	}
-	
+
 	IEnumerator MakeShit()
 	{
 		yield return new WaitForSeconds (startWait);
 
 		while (true)
 		{
-			Vector2 shitPosition = new Vector2 (gameObject.transform.x - distanceXFromCatAss, gameObject.transform.y);
-			Instantiate(shit, shitPosition);
+			Vector2 shitPosition = new Vector2 (gameObject.transform.position.x - distanceXFromProducerAss, gameObject.transform.position.y);
+			Instantiate(shit, shitPosition, Quaternion.identity);
+			shitScale = sizeShitFactor * 0.5f;
+			shit.transform.localScale = new Vector3(shitScale, shitScale, shitScale);
 			yield return new WaitForSeconds (makeShitWait);
 		}
 	}
